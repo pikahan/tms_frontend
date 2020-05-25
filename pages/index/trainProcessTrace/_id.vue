e50a89d903576e05e86448c6480e15985c5c908f
wavesheep <1196046520@qq.com>
2020-05-25 21:46:05 +0800

    add TrainProcessTrace

---
+++ pages/index/trainProcessTrace/_id.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h1>共计训练工夹具种类</h1>
+    <p>{{ detail.TotalToTrain }}</p>
+    <h1>训练起始时间</h1>
+    <p>{{ (new Date(Date.parse(detail.StartTime))).toLocaleString() }} - {{ (new Date(Date.parse(detail.finishTime))).toLocaleString() }}</p>
+    <h1 style="line-height: 30px">训练失败的工夹具定义及其原因</h1>
+    <a-list item-layout="horizontal" :data-source="failedLogs">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <a-list-item-meta :description="item.description">
+          <a slot="title">{{ item.title }}</a>
+        </a-list-item-meta>
+      </a-list-item>
+    </a-list>
+  </div>
+</template>
+
+<script>
+import trainLogGql from "@/apollo/queries/trainLog.gql";
+
+export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+      time: "",
+      detail: null,
+      failedLogs: []
+    };
+  },
+  created() {
+    const data = this.$apolloProvider.defaultClient
+      .query({
+        query: trainLogGql,
+        variables: {
+          id: this.$route.params.id
+        }
+      })
+      .then(({ data }) => {
+        console.log(data);
+        this.time = data.trainLog.time;
+        this.detail = JSON.parse(data.trainLog.detail);
+        for (const key in this.detail.FailedLogs) {
+          this.failedLogs.push({
+            title: "工夹具定义Id-"+key,
+            description: this.detail.FailedLogs[key]
+          })
+        }
+      })
+      .catch(e => {
+        console.error(e);
+      });
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ pages/index/trainProcessTrace/index.vue
@@ -0,0 +1,121 @@
+<template>
+  <div>
+    <a-radio-group v-model="mode" :style="{ marginBottom: '8px' }">
+      <a-radio-button value="training">正在训练</a-radio-button>
+      <a-radio-button value="history">历史训练记录</a-radio-button>
+    </a-radio-group>
+    <div v-show="mode === 'training'">
+      <h1>当前进度</h1>
+      <a-tooltip :title="title">
+        <a-progress :percent="trainPercent" :success-percent="trainSuccessPercent" status="active" />
+      </a-tooltip>
+      <h1 style="line-height: 30px; margin-top: 20px">失败的工夹具定义及其原因</h1>
+      <a-list item-layout="horizontal" :data-source="failedLogs">
+        <a-list-item slot="renderItem" slot-scope="item">
+          <a-list-item-meta
+            :description="item.description"
+          >
+            <a slot="title">{{ item.title }}</a>
+          </a-list-item-meta>
+        </a-list-item>
+      </a-list>
+    </div>
+    <div v-show="mode === 'history'">
+      <a-table :columns="columns" :dataSource="tableData" rowKey="id"  >
+        <div slot="StartTime" slot-scope="text">
+          {{ (new Date(Date.parse(text))).toLocaleString() }}
+        </div>
+        <div slot="FinshTime" slot-scope="text">
+          {{ (new Date(Date.parse(text))).toLocaleString() }}
+        </div>
+      </a-table>
+
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import TrainProcessManager from "@/util/trainProcessManager.ts";
+import { mapState } from "vuex";
+import query from '@/apollo/queries/allLogs.gql'
+
+const columns = [
+  { title: 'ID', dataIndex: 'id', key: 'id'},
+  { title: '开始时间', dataIndex: 'StartTime', key: 'StartTime', sorter: true, scopedSlots: { customRender: 'StartTime' }  },
+  { title: '结束时间', dataIndex: 'FinshTime', key: 'FinshTime', sorter: true , scopedSlots: { customRender: 'FinshTime' }  },
+  { title: '共计训练工夹具定义', dataIndex: 'TotalToTrain', key: 'TotalToTrain', sorter: true },
+
+];
+export default Vue.extend({
+  data() {
+    return {
+      ws: null,
+      mode: "training",
+      trainPercent: 0,
+      trainSuccessPercent: 0,
+      title: "0 success / 0 failed / 0 to train",
+      failedLogs: [],
+      data: [],
+      columns
+    };
+  },
+  created() {
+    this.fetchData()
+    let ws = new TrainProcessManager();
+    ws.addListener(this.userInfo.workcellId, data => {
+      console.log(data);
+      this.trainPercent = Number.parseFloat(
+        ((data.CurrentTrained / data.TotalToTrain) * 100).toFixed(2)
+      );
+      this.trainSuccessPercent =
+        ((data.CurrentTrained - data.FailedIds.length) / data.TotalToTrain) *
+        100;
+      this.title = `${data.CurrentTrained - data.FailedIds.length} success / ${
+        data.FailedIds.length
+      } failed / ${data.TotalToTrain - data.CurrentTrained} to train`;
+      let currentLogs = []
+      for (const key in data.FailedLogs) {
+        currentLogs.push({
+          title: "工夹具定义Id-"+key,
+          description: data.FailedLogs[key]
+        });
+      }
+      this.failedLogs = currentLogs;
+      if(data.TotalToTrain == data.CurrentTrained) {
+        this.trainPercent = 0;
+        this.trainSuccessPercent = 0;
+        this.failedLogs = [],
+        this.title = "0 success / 0 failed / 0 to train"
+      }
+    });
+    this.ws = ws;
+  },
+  methods: {
+    fetchData() {
+      this.$apolloProvider.defaultClient.query({
+        query: query,
+        variables: { type: "train"}
+      }).then(({data}) => {
+        console.log(data.trainLogs.payload)
+        this.data = data.trainLogs.payload
+      })
+    }
+  },
+  computed: {
+    ...mapState("user", ["userInfo"]),
+    tableData() {
+      return this.data.map(datum => {
+        let detail = JSON.parse(datum.detail)
+        return {
+          id: datum.id,
+          ...detail
+        }
+      })
+    }
+  }
+});
+</script>
+
+<style>
+</style>
